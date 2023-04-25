df3ff7d3c183a78dd038c9d4eb5dd88d8692def8
Ruben <rgolfe81@gmail.com>
2023-04-25 22:38:33 +0200

    feat: install Redux and add store.js and userSlice.js files

---
+++ src/app/store.js
@@ -0,0 +1,22 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userSlice from '../pages/userSlice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const reducers = combineReducers({
+    user: userSlice,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default configureStore({
+    reducer: persistedReducer,
+    middleware: [thunk]
+});
