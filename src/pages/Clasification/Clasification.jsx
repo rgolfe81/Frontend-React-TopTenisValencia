2a08a0387884ee4226e063c76da3af9777d4206b
Ruben <rgolfe81@gmail.com>
2023-05-02 14:31:04 +0200

    style: basic style in Clasification component with Tabla react-bootstrap

--- src/pages/Clasification/Clasification.jsx
+++ src/pages/Clasification/Clasification.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router'
 import { useSelector } from 'react-redux';
 import { tournamentIdData } from '../tournamentSlice';
 import { bringClassification, bringUsersForClassification } from '../../services/apiCalls';
+import { Table } from 'react-bootstrap';
 
 export const Clasification = () => {
     const navigate = useNavigate();
@@ -14,7 +15,6 @@ export const Clasification = () => {
     const formatedStartDateTournament = new Date(start_date).toLocaleDateString("es-ES");
     const formatedEndDateTournament = new Date(end_date).toLocaleDateString("es-ES");
     const [classification, setClassification] = useState([]);
-    // const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -22,11 +22,9 @@ export const Clasification = () => {
             try {
                 const usersResponse = await bringUsersForClassification (selectedTournamentId);
                 const users = usersResponse.data.data.users;
-                // setUsers(users);
 
                 const classificationResponse = await bringClassification (selectedTournamentId);
                 const clasification = classificationResponse.data.data;
-                // setClassification(clasification);
 
                 // Creamos un nuevo array con la información del nombre y apellidos del usuario para mostrar en el renderizado
                 const classificationWithUsers = clasification.map((ranking) => {
@@ -50,9 +48,55 @@ export const Clasification = () => {
         }
         fetchData()
     }, [selectedTournamentId]);
-    // console.log (users)
     console.log (classification)
+
   return (
-    <div>Clasification</div>
+    <div className='classificationDesign'>
+        <div className='titleClassification'>CLASIFICACIÓN</div>
+        <div className='titleTournamentClassification'>
+            <div>{selectedtournamentName}</div>
+            <div className='titleDataTournament'>{`${formatedStartDateTournament} a ${formatedEndDateTournament}`}</div>
+        </div>
+        <Table striped bordered className='bg-white border-3 tableClassification'>
+            <thead>
+                <tr className="titleRowTable text-center">
+                    <th>Jugador</th>
+                    <th>Puntos</th>
+                    <th>Jugados</th>
+                    <th>Ganados</th>
+                    <th>Perdidos</th>
+                </tr>
+            </thead>
+        <tbody>
+        {loading ? (
+            <tr>
+              <td colSpan={5}>Cargando datos ...</td>
+            </tr>
+          ) : classification.length > 0 ? (
+            classification.map((clasific) => (
+              <tr key={clasific.id}>
+                <td>{`${clasific.name} ${clasific.surname}`}</td>
+                <td className='text-center'>{clasific.score}</td>
+                <td className='text-center'>{clasific.matches_played}</td>
+                <td className='text-center'>{clasific.matches_win}</td>
+                <td className='text-center'>{clasific.matches_lost}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={5}>No se encuentran datos</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+      <button
+        className="btnTennisMatches"
+        onClick={() => {
+          navigate("/selectedTournament");
+        }}
+      >
+        Volver
+      </button>
+    </div>
   )
 }
