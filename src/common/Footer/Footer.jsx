2c4d868a95a85056579644d96d2d5a3a82106b45
Ruben <rgolfe81@gmail.com>
2023-05-05 22:06:33 +0200

    style: footer content added

--- src/common/Footer/Footer.jsx
+++ src/common/Footer/Footer.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./Footer.css"
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
 
 export const Footer = () => {
   return (
     <div className='footerDesign'>
+        <div className='boxTriSection'>
+            <div className='textTTV'>Top Tenis Valencia</div>
+            <div>
+                <div>Rubén Golfe Silvestre</div>
+                <div>Vilamarxant - Valencia</div>
+            </div>
+            <div>
+                <div>
+                    <BsGithub className='iconFooter'/> <a href="https://github.com/rgolfe81" className='text-white text-decoration-none'> my GitHub</a>
+                </div>
+                <div>
+                    <BsLinkedin className='iconFooter'/> <a href="https://www.linkedin.com/in/ruben-golfe/" className='text-white text-decoration-none'>my LinkedIn</a>
+                </div>
+            </div>
+        </div>
+        <div className='textGHA'>Proyecto final de formación bootcamp Full Stack Developer en GeeksHubs Academy</div>
 
     </div>
   )
